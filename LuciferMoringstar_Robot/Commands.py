a626a9fc175cd93db3817f02533f01f8cd397021
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-10-18 23:23:53 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -45,7 +45,7 @@ async def start(bot, message):
                 ident, file_id = message.text.split("_-_-_-_")
                 await bot.send_message(
                     chat_id=message.from_user.id,
-                    text="**നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ ഇതുവരെ ജോയിൻ ചെയ്തിട്ടില്ല ജോയിൻ ചെയ്തതിന് ശേഷം വീണ്ടും ശ്രമിക്കുക\n\n[ You Have Not Yet Joined Our Channel  Please Try Again After Joining ]**",
+                    text="**   നിങ്ങൾ ഞങ്ങളുടെ ചാനലിൽ ഇതുവരെ ജോയിൻ ചെയ്തിട്ടില്ല ജോയിൻ ചെയ്തതിന് ശേഷം വീണ്ടും ശ്രമിക്കുക\n\n[ You Have Not Yet Joined Our Channel  Please Try Again After Joining ]**",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
