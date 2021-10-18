2b6a3f5fc255778e1838d9451808d7ebe7e89526
NewCreate <85439835+NewCreate@users.noreply.github.com>
2021-10-18 22:22:31 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,9 +89,9 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Deploy Video', url=f'{TUTORIAL}')
+        InlineKeyboardButton('Search again 🔎', switch_inline_query_current_chat=query)
         ],[
-        InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
+        InlineKeyboardButton('〽️ New Release', url='https://t.me/joinchat/aG4_u8iHx31mNzVl')
         ]]
     return InlineKeyboardMarkup(buttons)
 
